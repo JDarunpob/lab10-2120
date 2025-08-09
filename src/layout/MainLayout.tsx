31a328376a6e445e0d04ee39d1ada935783165a8
JDarunpob <darunpob.jean@gmail.com>
2025-08-09 16:13:23 +0700

    fish

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -20,7 +20,7 @@ export default function MainLayout() {
           </main>
 
           {/* Footer ด้านล่าง */}
-          <Footer year="2026" fullName="chanadda" studentId="67062039" />
+          <Footer year="2025" fullName="Darunpob Saengson" studentId="670612120" />
         </div>
       </div>
     </div>
